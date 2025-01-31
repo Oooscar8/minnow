77c6e78236e84f5442c04f899467cfb9d1cc652f
Alex Sun <oscar8@sjtu.edu.cn>
2025-01-31 07:39:19 +0000

    Use a while loop in unwrap to find the closest seq. Timeout!

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -6,13 +6,28 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  debug( "wrap( {}, {} ) called", n, zero_point.raw_value_ );
+
+  return Wrap32( static_cast<uint32_t>( ( zero_point.raw_value_ + n ) % ((uint64_t)UINT32_MAX + 1) ) );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  debug( "Wrap32( {} ).unwrap( {}, {} ) called", raw_value_, zero_point.raw_value_, checkpoint );
+
+  uint32_t offset = ( raw_value_ >= zero_point.raw_value_ )
+                      ? raw_value_ - zero_point.raw_value_
+                      : raw_value_ + UINT32_MAX - zero_point.raw_value_ + 1;
+
+  uint64_t seqnos = offset;
+  uint64_t difference = UINT64_MAX;
+  while ( true ) {
+    if ( seqnos >= checkpoint ) {
+      uint64_t last_seqno = seqnos - ((uint64_t)UINT32_MAX + 1);
+      return ( seqnos - checkpoint < difference ) ? seqnos : last_seqno;
+    }
+    difference = checkpoint - seqnos;
+    seqnos = seqnos + UINT32_MAX + 1;
+  }
 }
