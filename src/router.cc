c08ff25361a3e2b2946055f6891c32bb5ed04a49
Alex Sun <oscar8@sjtu.edu.cn>
2025-02-26 12:00:17 +0000

    Fixed bugs in syntax

--- src/router.cc
+++ src/router.cc
@@ -30,7 +30,7 @@ void Router::add_route( const uint32_t route_prefix,
     next_hop_ip = next_hop->ipv4_numeric();
   }
 
-  routing_table_.insert( make_pair( route_prefix, prefix_length ), make_pair( next_hop_ip, interface_num ) );
+  routing_table_.insert( { make_pair( route_prefix, prefix_length ), make_pair( next_hop_ip, interface_num ) } );
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
